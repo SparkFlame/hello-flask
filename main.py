528916398d518aa78162e4a1719db66ccda9cf61
SparkFlame <k.hora2003@gmail.com>
2016-03-25 19:42:54 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
 #flask example
+
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return render_template("hello.html")
+    
+@app.route("/about/")
+def about():
+  return render_template("about.html")
+  
+@app.route("/main/")
+def main():
+  return ("main.html")
+  
+  
+if __name__ == "__main__":
+    app.run()
