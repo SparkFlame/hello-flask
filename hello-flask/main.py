027b9ec74beca463073f26751e3e8145c213cc64
SparkFlame <k.hora2003@gmail.com>
2016-03-26 13:18:11 -0600

    Update main.py

--- hello-flask/main.py
+++ hello-flask/main.py
@@ -4,7 +4,7 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
+def mainpage():
     return render_template("hello.html")
     
 @app.route("/about/")
@@ -15,6 +15,9 @@ def about():
 def main():
   return ("main.html")
   
+"""@app.route("/main/welcome/<name>")
+def welcome(name=None):
+    return render_template('welcome.html', name=name)"""
   
 if __name__ == "__main__":
     app.run()
